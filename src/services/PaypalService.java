0fd0291ea1c47a04f779a94d170be685708ec93f
Filipe Moura <flp.smr@gmail.com>
2022-08-10 03:26:12 -0300

    Changed constant names to match Java's naming convention

--- src/services/PaypalService.java
+++ src/services/PaypalService.java
@@ -2,18 +2,18 @@ package services;
 
 public class PaypalService implements OnlinePaymentService {
 	
-	private static final double fee = 0.02;
-	private static final double interest = 0.01;
+	private static final double FEE = 0.02;
+	private static final double INTEREST = 0.01;
 
 	@Override
 	public double interest(double amount, int months) {
-		return amount * interest * months;
+		return amount * INTEREST * months;
 		
 	}
 	
 	@Override
 	public double paymentFee(double amount) {
-		return amount * fee;
+		return amount * FEE;
 	}
 
 	
